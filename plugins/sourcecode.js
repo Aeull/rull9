2fb92e078ff04d27155cdc5927959eb9d98a798b
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-26 08:24:39 +0800

    Update sourcecode.js

--- plugins/sourcecode.js
+++ plugins/sourcecode.js
@@ -1,19 +1,23 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, 'https://telegra.ph/file/18c123b72250f8f44a6b8.jpg',
-`Hi Kak @${m.sender.split('@')[0]} 
+let handler = async (m, { conn }) => conn.send2ButtonDoc(m.chat, `Hi Kak @${m.sender.split('@')[0]} 
 
 ◪ 📮 *SCRIPT BOT*
 │ *Script :* 
-│ ╰ https://github.com/sadteams/bot-md
+│ ╰ https://youtu.be/Y4lDHozWsnI
 │ *Base :* 
 │ ╰ https://github.com/Fokusdotid/Family-MD
 ╰──────────═┅═──────────
-`,`📍 *N o t e :* 
+` ,'📍 *N o t e :* 
 • Jangan lupa minta izin owner sebelum menggunakan scriptnya kak!
-• Jangan Lupa kasih star & kasih credit
-
-Official By @${'0'.split('@')[0]}
-Powered By @${`${global.owner[0]}`.split('@')[0]}`, 'Pemilik Bot', '#owner')
+• Jangan Lupa kasih star & kasih credit', 'Pemilik Bot', '#owner', 'Menu', '#menu', m, { contextInfo: { externalAdReply :{
+  showAdAttribution: true,
+  sourceUrl: 'https://youtu.be/Y4lDHozWsnI',
+  mediaUrl: 'https://youtu.be/Y4lDHozWsnI',
+  mediaType: 'https://youtu.be/Y4lDHozWsnI',
+  thumbnail: await(await fetch(img)).buffer(),
+  body: wm,
+  title: 'Tuh Kak Scnya',
+  }}})
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
