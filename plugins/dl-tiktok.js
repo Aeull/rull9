567a93325ed858d96ef3c79b88d3bc4394d99027
Aeull <110038479+Aeull@users.noreply.github.com>
2022-09-27 09:27:53 +0800

    Update dl-tiktok.js

--- plugins/dl-tiktok.js
+++ plugins/dl-tiktok.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
 if (!args[0]) throw 'Uhm..url nya mana?'
 m.reply(wait)
-let res = await fetch(`https://botcahx-rest-api.up.railway.app/api/dowloader/tikok?url=${args[0]}`)
+let res = await fetch(`https://api.lolhuman.xyz/api/tiktokwm?apikey=7facd6f11077ee4daecd55d5&url=${args[0]}`)
 if (!res.ok) throw await res.text()
 let json = await res.json()
 if (!json.status) throw json
